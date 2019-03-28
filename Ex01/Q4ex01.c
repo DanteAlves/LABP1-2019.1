4e92a403e305afa55a4c8fe79273efe5730c4119
André Alves <dante.alves1999@gmail.com>
2019-03-28 19:13:32 -0300

    Create Q4ex01.c

---
+++ Ex01/Q4ex01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+  puts("AAAAAAAAA");
+  puts("A   A    ");
+  puts("A   A    ");
+  puts("A   A    ");
+  puts("AAAAAAAAA");
+
+  puts("\n");
+
+  puts("AAAAAAAAA");
+  puts("A   A    ");
+  puts("A   A    ");
+  puts("A   A    ");
+  puts("AAAAAAAAA");
+
+  puts("\n");
+
+  puts("B B   B B");
+  puts("B   B   B");
+  puts("B   B   B");
+  puts("B       B");
+  puts("BBBBBBBBB");
+  return 0;
+}
