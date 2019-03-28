2afe851854cfb06b0a803dd11c842d0da3e0a740
André Alves <dante.alves1999@gmail.com>
2019-03-28 19:13:34 -0300

    Create Q5ex01.c

---
+++ Ex01/Q5ex01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  printf(" * * * * * * * *");
+  printf("\n");
+  printf("* * * * * * * *");
+  printf("\n");
+  printf(" * * * * * * * *");
+  printf("\n");
+  printf("* * * * * * * *");
+  printf("\n");
+  printf(" * * * * * * * *");
+  printf("\n");
+  printf("* * * * * * * *");
+  printf("\n");
+  printf(" * * * * * * * *");
+  printf("\n");
+  printf("* * * * * * * *");
+  return 0;
+}
